b5f7ca398f187bff3891cd8e11a8bbb30b221877
Andre Gomez <andremgm1@hotmail.com>
2020-02-11 16:31:33 -0600

    pushing first time

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace indexs
+{
+
+    public class Logger
+    {
+
+        public void log(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+    public class Navegator
+    {
+        public void WhichNavegator()
+        {
+            Console.WriteLine("estas en explorer");
+        }
+
+    }
+    public class HttpCookie : Navegator
+    {
+        private Dictionary<string, string> _dictionary;
+        private Logger looger = new Logger();
+
+
+        public HttpCookie()
+        {
+            _dictionary = new Dictionary<string, string>();
+
+        }
+
+        public string this[string key]
+        {
+            get
+            {
+                return _dictionary[key];
+            }
+
+            set
+            {
+                _dictionary[key] = value;
+                looger.log("this is a test");
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cookie = new HttpCookie();
+
+            cookie["name"] = "Andre";
+
+            Console.WriteLine(cookie["name"]);
+            cookie.WhichNavegator();
+            
+
+        }
+    }
+}
